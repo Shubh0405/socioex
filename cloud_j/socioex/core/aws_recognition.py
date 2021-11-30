1693f0761c4aa0ccd8b61e5830dc2d43b9f918f3
Akshat1903 <akshatgupta1903@gmail.com>
2021-11-30 18:53:13 +0530

    rekognition added

--- cloud_j/socioex/core/aws_recognition.py
+++ cloud_j/socioex/core/aws_recognition.py
@@ -1,18 +1,27 @@
-import csv
 import boto3
 from variables import CONFIG
+from PIL import Image
+import requests
+from io import BytesIO
 
 photo = "image.jpg"
 
-client = boto3.client('rekognition',
+reko_client = boto3.client('rekognition',
                     region_name='ap-south-1',
                     aws_access_key_id=CONFIG["AWS_ACCESS_KEY"],
                     aws_secret_access_key=CONFIG["AWS_SECRET_KEY"])
 
-async def get_recognition(image):
+def get_image_recognition(image):
     with open(image, 'rb') as source_image:
         source_bytes = source_image.read()
 
-    response = client.detect_labels(Image = {'Bytes': source_bytes}, MaxLabels = 5, MinConfidence = 95)
+    response = reko_client.detect_labels(Image = {'Bytes': source_bytes}, MaxLabels = 5, MinConfidence = 95)
+    return response
+
+def get_image_url_recognition(url):
+    response = requests.get(url)
+    img = BytesIO(response.content).read()
+
+    response = reko_client.detect_labels(Image = {'Bytes': img}, MaxLabels = 5, MinConfidence = 95)
     return response
 
--- cloud_j/socioex/views/twitter/methods.py
+++ cloud_j/socioex/views/twitter/methods.py
@@ -3,7 +3,7 @@ from bson import json_util
 from fastapi import Query, APIRouter, Response, status, Request
 from core.connections import export_db_class
 from core.fetch_twitter import get_tweets
-from core.aws_recognition import get_recognition
+from core.aws_recognition import get_image_recognition, get_image_url_recognition
 from variables import CONFIG
 
 twitter_router = APIRouter() 
@@ -37,21 +37,18 @@ async def get_user_data(request:Request, user: str = Query(None)):
 async def get_user_twitter(request:Request, user: str = Query(None)):
     data = get_tweets(user)
 
+    for d in data:
+        d["text"] = translate.translate_text(Text=d["text"], SourceLanguageCode="auto", TargetLanguageCode="en")["TranslatedText"]
+        if d["image"]:
+            d["image_labels"] = get_image_url_recognition(d["image"])
+
     data = json_util.dumps(data)
 
     return Response(content = data, media_type="application/json", status_code= status.HTTP_200_OK)
 
 @twitter_router.get('/get-recognition')
-async def get_recognition(request:Request):
-    image = "images/01.jpg"
-    data = get_recognition(image)
-    data.__next__()
-
-    print(data)
-
-    data = {
-        "adas": "asdsa"
-    }
+async def get_recognition(request:Request, url: str = Query(None)):
+    data = get_image_url_recognition(url)
 
     data = json_util.dumps(data)
 
